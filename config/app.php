dacc855659391e3eebc774a0e4dbb94cee21e7f8
masadi <masadi.com@gmail.com>
2025-11-11 13:39:10 +0700

    Ganti nama aplikasi

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => 'SIPENA',
+    'name' => 'SIPENA SMK',
     'version' => env('APP_VERSION', '8.0.0'),
     'registration' => env('APP_REGISTRATION', FALSE),
     'github_token' => env('APP_GITHUB_TOKEN', ''),
