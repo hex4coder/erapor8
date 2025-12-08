a6958aa4ad1720a0c980652f74da2c2304f2c65d
Muhammad Farras Rayhand <35839373+farrasrayhand@users.noreply.github.com>
2025-12-08 17:14:15 +0800

    Fix loading lama saat cetak rapor

--- resources/views/layouts/cetak.blade.php
+++ resources/views/layouts/cetak.blade.php
@@ -10,7 +10,7 @@
     <title>Cetak Dokumen</title>
     <!-- Styles -->
 	<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/cetak.css') }}" />
+    <link rel="stylesheet" href="{{ public_path('css/cetak.css') }}" />
     <style>
         body { 
             font-family: Tahoma;
